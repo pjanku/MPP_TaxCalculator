b005480bcdac280d0d000f245e419c7691ba7fd5
Peter Janků <peter.janku@gmail.com>
2024-10-14 07:20:59 +0200

    DELETE unused translate files. ADD new translate files. (without translations)

--- SimpleTaxCalc_cs.ts
+++ SimpleTaxCalc_cs.ts
@@ -1,47 +1,47 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.1" language="cs_CZ" sourcelanguage="en">
+<TS version="2.1" language="cs" sourcelanguage="en">
 <context>
     <name>MainWindow</name>
     <message>
         <location filename="mainwindow.ui" line="14"/>
         <source>MainWindow</source>
-        <translation>Hlavní okno</translation>
+        <translation> Hlavní okno</translation>
     </message>
     <message>
         <location filename="mainwindow.ui" line="23"/>
         <source>Without TAX:</source>
-        <translation>Bez DPH:</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="mainwindow.ui" line="33"/>
         <source>With TAX:</source>
-        <translation>S DPH:</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="mainwindow.ui" line="60"/>
         <source>TAX level:</source>
-        <translation>DPH:</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="mainwindow.ui" line="66"/>
         <source>15 %</source>
-        <translation></translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="mainwindow.ui" line="73"/>
         <source>21 %</source>
-        <translation></translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="mainwindow.ui" line="86"/>
         <source>Clear</source>
-        <translation>Vymazat</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="mainwindow.ui" line="118"/>
         <source>File</source>
-        <translation>Soubor</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="mainwindow.ui" line="124"/>
@@ -51,17 +51,17 @@
     <message>
         <location filename="mainwindow.ui" line="135"/>
         <source>Quit</source>
-        <translation>Konec</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="mainwindow.ui" line="143"/>
         <source>English</source>
-        <translation>Angličtina</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="mainwindow.ui" line="151"/>
         <source>Czech</source>
-        <translation>Čeština</translation>
+        <translation type="unfinished"></translation>
     </message>
 </context>
 </TS>
